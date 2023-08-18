ca59642b85c2643d49ed5c8fd6e7ddfeb6d4ef13
Hendra Kusumah <h.kusumah87@gmail.com>
2023-08-19 05:35:39 +0700

    Add files via upload

---
+++ birdwatcher_stm32/birdwatcher_stm32.ino
@@ -0,0 +1,201 @@
+
+#include <Arduino.h>
+#include "Ethernet.h"
+#include "Seeed_Arduino_GroveAI.h"
+#include <Wire.h>
+
+#define SERVER_PORT 80
+GroveAI ai(Wire);
+uint8_t state = 0;
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(SERVER_PORT);
+
+void setup() {
+  //STM32F429ZI's Serial port changed from default Serial Port
+  Serial2.setRx(PC11);
+  Serial2.setTx(PC10);  
+  Serial2.begin(9600);
+  Serial.println("Ethernet WebServer Example");
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+    print_network_info();
+  
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+
+  // start the server
+  server.begin();
+  Serial2.print("server is at ");
+  Serial2.println(Ethernet.localIP());
+  Wire.begin();
+
+   Serial.println("begin");
+  if (ai.begin(ALGO_OBJECT_DETECTION, MODEL_EXT_INDEX_1)) // Object detection and pre-trained model 1
+  {
+    Serial.print("Version: ");
+    Serial.println(ai.version());
+    Serial.print("ID: ");
+    Serial.println( ai.id());
+    Serial.print("Algo: ");
+    Serial.println( ai.algo());
+    Serial.print("Model: ");
+    Serial.println(ai.model());
+    Serial.print("Confidence: ");
+    Serial.println(ai.confidence());
+    state = 1;
+  }
+  else
+  {
+    Serial.println("Algo begin failed.");
+  }
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          client.println("<HEAD>");
+          client.println("<meta name='apple-mobile-web-app-capable' content='yes' />");
+          client.println("<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />");
+          client.println("<link rel='stylesheet' type='text/css' href='https://randomnerdtutorials.com/ethernetcss.css' />");
+          client.println("<TITLE>TinyML Bird Watcher</TITLE>");
+          client.println("</HEAD>");
+          client.println("<BODY>");
+          client.println("<H1>Reading Result</H1>");
+          // output the value of each analog input pin
+           if (state == 1)
+  {
+    uint32_t tick = millis();
+    if (ai.invoke()) // begin invoke
+    {
+      while (1) // wait for invoking finished
+      {
+        CMD_STATE_T ret = ai.state(); 
+        if (ret == CMD_STATE_IDLE)
+        {
+          break;
+        }
+        delay(20);
+      }
+
+     uint8_t len = ai.get_result_len(); // receive how many people detect
+     if(len)
+     {
+       int time1 = millis() - tick; 
+       Serial.print("Time consuming: ");
+       Serial.println(time1);
+       client.print("Number of birds: ");
+       client.print(len);
+       Serial.print("Number of birds: ");
+       Serial.println(len);
+       object_detection_t data;       //get data
+
+       for (int i = 0; i < len; i++)
+       {
+          Serial.println("result:detected");
+          Serial.print("Detecting and calculating: ");
+          Serial.println(i+1);
+          ai.get_result(i, (uint8_t*)&data, sizeof(object_detection_t)); //get result
+  
+          Serial.print("confidence:");
+          Serial.print(data.confidence);
+          Serial.println();
+        }
+     }
+     else
+     {
+       Serial.println("No identification");
+       client.print("No identification");
+     }
+     client.println("<br />");
+    }
+    else
+    {
+      delay(1000);
+      Serial.println("Invoke Failed.");
+    }
+  }
+  else
+  {
+    state == 0;
+  }
+             
+          
+          
+      
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void print_network_info(void){
+  byte print_mac[] ={0,};
+  Serial.println("\r\n-------------------------------------------------");
+  Serial.printf("MAC        : ");
+  Ethernet.MACAddress(print_mac);
+  for (byte i = 0; i < 6; i++) {
+    Serial.print(print_mac[i], HEX);
+    if (i < 5) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+  Serial.printf("IP         : ");
+  Serial.print(Ethernet.localIP());
+  Serial.printf(": %d\r\n", SERVER_PORT);
+  Serial.println("-------------------------------------------------");
+}
